ae3a56362b7df649a37c01640f638207d292836e
Daniel Diaz Granizo <daniel.diazg@scmspain.com>
2020-07-16 17:23:47 +0200

    added code behind for component and added namespace to _imports.razor

---
+++ Source/DD.Youtube.HyperNotesPlus.Shared/Utilities/StringUtilities.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DD.Youtube.HyperNotesPlus.Shared.Utilities
+{
+    public static class StringUtilities
+    {
+        public static string ToUpper(string text)
+        {
+            return text.ToUpper();
+        }
+    }
+}
