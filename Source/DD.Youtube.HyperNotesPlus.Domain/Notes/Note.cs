d749913804fd2ff8b36e77168b3b8c5934daf576
Daniel Diaz Granizo <daniel.diazg@scmspain.com>
2020-07-16 18:07:32 +0200

    created domain proyects, created Note model, created NotesListComponent

---
+++ Source/DD.Youtube.HyperNotesPlus.Domain/Notes/Note.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using static DD.Youtube.HyperNotesPlus.Domain.Common.Definitions;
+
+namespace DD.Youtube.HyperNotesPlus.Domain.Notes
+{
+    public class Note
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Content { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public StateEnum State { get; set; }
+    }
+}
