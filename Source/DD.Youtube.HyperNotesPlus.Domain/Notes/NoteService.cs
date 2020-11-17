d13e243c2f7f252c282ecc99a217a0f8ac4e0114
Daniel Diaz Granizo <daniel.diazg@scmspain.com>
2020-11-17 18:37:32 +0100

    updated service with list, added render fragment for zero records

--- Source/DD.Youtube.HyperNotesPlus.Domain/Notes/NoteService.cs
+++ Source/DD.Youtube.HyperNotesPlus.Domain/Notes/NoteService.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -7,44 +8,44 @@ namespace DD.Youtube.HyperNotesPlus.Domain.Notes
 {
     public class NoteService : INoteService
     {
+        public List<Note> Notes { get; set; }
+
         public NoteService()
         {
+            Notes = new List<Note>();
         }
 
         public async Task<Guid> Create(Note note)
         {
+            note.Id = Guid.NewGuid();
+            note.State = Common.Definitions.StateEnum.Enabled;
+            Notes.Add(note);
             await Task.Delay(200);
-            return Guid.NewGuid();
+            return note.Id;
         }
 
         public async Task Update(Guid id, Note note)
         {
             await Task.Delay(200);
+            var noteInList = Notes.FirstOrDefault(k => k.Id == id)
+                ?? throw new ArgumentOutOfRangeException($"Can't find record with Id={id}");
+            var index = Notes.IndexOf(noteInList);
+            Notes[index] = note;
         }
 
 
         public async Task<Note> GetNote(Guid id)
         {
             await Task.Delay(200);
-            return new Note()
-            {
-                Id = id,
-                Content = "TestContent",
-                Name = "Name test",
-                State = Common.Definitions.StateEnum.Enabled,
-                CreatedOn = DateTime.Now,
-            };
+            var note = Notes.FirstOrDefault(k => k.Id == id)
+                ?? throw new ArgumentOutOfRangeException($"Can't find record with Id={id}");
+            return note;
         }
 
         public async Task<IEnumerable<Note>> GetAllNotes()
         {
             await Task.Delay(200);
-            return new List<Note>()
-            {
-                {new Note{ Id = Guid.NewGuid(), Name = "Nota1", Content = "Contenido nota 1", State = Domain.Common.Definitions.StateEnum.Enabled }},
-                {new Note{ Id = Guid.NewGuid(), Name = "Nota2", Content = "Contenido nota 2", State = Domain.Common.Definitions.StateEnum.Disabled }},
-                {new Note{ Id = Guid.NewGuid(), Name = "Nota3", Content = "Contenido nota 3", State = Domain.Common.Definitions.StateEnum.Enabled }},
-            };
+            return Notes;
         }
     }
 }
